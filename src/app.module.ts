7ea40485591125c0c632a5f658ac2ffe4f536592
Duduzera006 <dudusapede@gmail.com>
2025-11-25 13:23:53 -0300

    ordersModule import correction

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { AuthModule } from './auth/auth.module.js';
 import { FirebaseModule } from '../firebase/firebase.module.js';
 import { ProductsModule } from './products/products.module.js';
 import { CloudinaryModule } from '../cloudinary/cloudinary.module.js';
+import { OrdersModule } from './orders/orders.module.js';
 
 @Module({
-  imports: [AuthModule, FirebaseModule, ProductsModule, CloudinaryModule],
+  imports: [AuthModule, FirebaseModule, ProductsModule, CloudinaryModule, OrdersModule],
   controllers: [AppController],
   providers: [AppService],
 })
